a7dda9a80d21865f6f0d488b2726fc0513aaecda
Hajime NAKAMURA <h.nakamura0903@gmail.com>
2020-05-17 17:08:57 +0900

    1255. Maximum Score Words Formed by Letters

---
+++ maximum_score_words_formed_by_letters/Solution.java
@@ -0,0 +1,31 @@
+package maximum_score_words_formed_by_letters;
+
+import java.util.stream.IntStream;
+
+public class Solution {
+    public int maxScoreWords(String[] words, char[] letters, int[] score) {
+        int[] a = new int[26];
+        for (char letter : letters) {
+            a[letter - 'a']++;
+        }
+        int n = words.length;
+        int ans = 0;
+        for (int bit = 0; bit < (1 << n); bit++) {
+            int[] b = new int[26];
+            for (int i = 0; i < n; i++) {
+                if ((bit & (1 << i)) != 0) {
+                    for (char ch : words[i].toCharArray()) {
+                        b[ch - 'a']++;
+                    }
+                }
+            }
+
+            boolean ok = IntStream.range(0, 26).allMatch(i -> a[i] >= b[i]);
+            if (ok) {
+                int sum = IntStream.range(0, 26).map(i -> b[i] * score[i]).sum();
+                ans = Math.max(ans, sum);
+            }
+        }
+        return ans;
+    }
+}
---
+++ maximum_score_words_formed_by_letters/SolutionTest.java
@@ -0,0 +1,41 @@
+package maximum_score_words_formed_by_letters;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class SolutionTest {
+    private Solution solution;
+
+    @BeforeEach
+    public void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    public void example1() {
+        String[] words = new String[]{"dog","cat","dad","good"};
+        char[] letters = new char[]{'a','a','c','d','d','d','g','o','o'};
+        int[] score = new int[]{1,0,9,5,0,0,3,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0};
+        assertEquals(23, solution.maxScoreWords(words, letters, score));
+    }
+
+    @Test
+    public void example2() {
+        String[] words = new String[]{"xxxz","ax","bx","cx"};
+        char[] letters = new char[]{'z','a','b','c','x','x','x'};
+        int[] score = new int[]{4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,0,10};
+        assertEquals(27, solution.maxScoreWords(words, letters, score));
+    }
+
+    @Test
+    public void example3() {
+        String[] words = new String[]{"leetcode"};
+        char[] letters = new char[]{'l','e','t','c','o','d'};
+        int[] score = new int[]{0,0,1,1,1,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0};
+        assertEquals(0, solution.maxScoreWords(words, letters, score));
+    }
+
+}

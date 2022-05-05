d9390b3ea824eb19e541f639297e3aa94f199fc7
neko_the_shadow <h.nakamura0903@gmail.com>
2022-05-05 14:50:50 +0900

    225. Implement Stack using Queues

---
+++ implement-stack-using-queues/main.py
@@ -0,0 +1,23 @@
+import imp
+
+
+class MyStack:
+
+    def __init__(self):
+        import collections
+        self.queue = collections.deque()
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+        for _ in range(len(self.queue)-1):
+            self.queue.append(self.queue.popleft())
+
+    def pop(self) -> int:
+        return self.queue.popleft()
+
+    def top(self) -> int:
+        return self.queue[0]
+        
+
+    def empty(self) -> bool:
+        return len(self.queue) == 0

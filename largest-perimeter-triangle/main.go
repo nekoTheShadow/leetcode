7a2ba21e26781ed03d62e87bca670f99661ca0d2
neko_the_shadow <h.nakamura0903@gmail.com>
2022-10-12 22:54:33 +0900

    976. Largest Perimeter Triangle

---
+++ largest-perimeter-triangle/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "sort"
+
+func largestPerimeter(nums []int) int {
+	sort.Ints(nums)
+	for i := len(nums) - 1; i > 1; i-- {
+		if nums[i] < nums[i-1]+nums[i-2] {
+			return nums[i] + nums[i-1] + nums[i-2]
+		}
+	}
+	return 0
+}

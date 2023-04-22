c0d8e22e35505b0bda23d7974fa771c6de3511c6
neko_the_shadow <h.nakamura0903@gmail.com>
2023-04-22 23:32:24 +0900

    516. Longest Palindromic Subsequence

---
+++ longest_palindromic_subsequence/Solution.java
@@ -0,0 +1,39 @@
+package longest_palindromic_subsequence;
+
+public class Solution {
+    public int longestPalindromeSubseq(String s) {
+        return lcs(s, new StringBuilder(s).reverse().toString()).length();
+    }
+    
+    public String lcs(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n+1][m+1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i-1) == t.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        int x = n;
+        int y = m;
+        StringBuilder u = new StringBuilder();
+        while(x > 0 && y > 0) {
+            if (dp[x][y] == dp[x-1][y]) {
+                x--;
+            } else if (dp[x][y] == dp[x][y-1]) {
+                y--;
+            } else {
+                x--;
+                y--;
+                u.append(s.charAt(x));
+            }
+        }
+
+        return u.reverse().toString();
+    }
+}

7b2874e183dfbe261ce7044065ffd10d6f4411f1
neko_the_shadow <h.nakamura0903@gmail.com>
2024-03-17 01:23:55 +0900

    525. Contiguous Array

--- contiguous-array/src/main.rs
+++ contiguous-array/src/main.rs
@@ -3,19 +3,26 @@ use std::collections::HashMap;
 impl Solution {
     pub fn find_max_length(nums: Vec<i32>) -> i32 {
         let n = nums.len();
-        let mut d = vec![0; n + 1];
+        let mut a = vec![0; n + 1];
+        let mut b = vec![0; n + 1];
         for i in 0..n {
-            d[i + 1] = d[i] + nums[i];
+            if nums[i] == 1 {
+                a[i + 1] = a[i] + 1;
+                b[i + 1] = b[i];
+            } else {
+                a[i + 1] = a[i];
+                b[i + 1] = b[i] + 1;
+            }
         }
 
         let mut h = HashMap::new();
         let mut max_len = 0;
-        for x in 0..=n {
-            let k = 2 * d[x] - (x as i32);
-            if let Some(y) = h.get(&k) {
-                max_len = max_len.max(x - y);
+        for y in 0..=n {
+            let k = a[y] - b[y];
+            if let Some(x) = h.get(&k) {
+                max_len = max_len.max(y - x);
             } else {
-                h.insert(k, x);
+                h.insert(k, y);
             }
         }
 

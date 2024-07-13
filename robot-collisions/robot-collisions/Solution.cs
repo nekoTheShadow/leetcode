bf70537d749d01c17737b37a3a7be479c4be8990
neko_the_shadow <h.nakamura0903@gmail.com>
2024-07-13 12:26:07 +0900

    2751. Robot Collisions

---
+++ robot-collisions/robot-collisions/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace robot_collisions;
+public class Solution
+{
+    public IList<int> SurvivedRobotsHealths(int[] positions, int[] healths, string directions)
+    {
+        var n = positions.Length;
+        var indices = Enumerable.Range(0, n).OrderBy(i => positions[i]).ToList();
+        var stack = new Stack<int>();
+        foreach (var current_index in indices)
+        {
+            if (directions[current_index] == 'R')
+            {
+                stack.Push(current_index);
+            } else
+            {
+                while (stack.Count > 0 && healths[current_index] > 0)
+                {
+                    var top_index = stack.Pop();
+                    if (healths[top_index] > healths[current_index])
+                    {
+                        healths[top_index]--;
+                        healths[current_index] = 0;
+                        stack.Push(top_index);
+                    } else if (healths[top_index] < healths[current_index]) 
+                    {
+                        healths[top_index] = 0;
+                        healths[current_index]--;
+                    } else
+                    {
+                        healths[top_index] = 0;
+                        healths[current_index] = 0;
+                    }
+                }
+            }
+        }
+
+        return Enumerable.Range(0, n).Where(i => healths[i] > 0).Select(i => healths[i]).ToList();
+    }
+}
---
+++ robot-collisions/robot-collisionsTests/SolutionTests.cs
@@ -0,0 +1,29 @@
+﻿namespace robot_collisions.Tests;
+
+[TestClass()]
+public class SolutionTests
+{
+    [TestMethod()]
+    public void Example1()
+    {
+        var actual = new Solution().SurvivedRobotsHealths(new int[] { 5, 4, 3, 2, 1 }, new int[] { 2, 17, 9, 15, 10 }, "RRRRR");
+        var expected = new List<int> { 2, 17, 9, 15, 10 };
+        CollectionAssert.AreEqual(expected, actual.ToArray());
+    }
+
+    [TestMethod()]
+    public void Example2()
+    {
+        var actual = new Solution().SurvivedRobotsHealths(new int[] { 3, 5, 2, 6 }, new int[] { 10, 10, 15, 12 }, "RLRL");
+        var expected = new List<int> { 14 };
+        CollectionAssert.AreEqual(expected, actual.ToArray());
+    }
+
+    [TestMethod()]
+    public void Example3()
+    {
+        var actual = new Solution().SurvivedRobotsHealths(new int[] { 1, 2, 5, 6 }, new int[] { 10, 10, 11, 11 }, "RLRL");
+        var expected = new List<int> { };
+        CollectionAssert.AreEqual(expected, actual.ToArray());
+    }
+}

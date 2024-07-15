5220c8acf90c5029b0ec0512250ae1ab79b8d2fa
neko_the_shadow <h.nakamura0903@gmail.com>
2024-07-15 15:01:34 +0900

    2196. Create Binary Tree From Descriptions

---
+++ create-binary-tree-from-descriptions/Program.cs
@@ -0,0 +1,6 @@
+﻿
+using create_binary_tree_from_descriptions;
+
+Console.WriteLine(new Solution().CreateBinaryTree(new int[][] { new int[] { 20, 15, 1 }, new int[] { 20, 17, 0 }, new int[] { 50, 20, 1 }, new int[] { 50, 80, 0 }, new int[] { 80, 19, 1 } }));
+Console.WriteLine(new Solution().CreateBinaryTree(new int[][] { new int[] { 1, 2, 1 }, new int[] { 2, 3, 0 }, new int[] { 3, 4, 1 } }));
+
---
+++ create-binary-tree-from-descriptions/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace create_binary_tree_from_descriptions;
+
+public class Solution
+{
+    public TreeNode CreateBinaryTree(int[][] descriptions)
+    {
+        var dict = new Dictionary<int, TreeNode>();
+        foreach (var description in descriptions)
+        {
+            var parent = description[0];
+            var child = description[1];
+            var isleft = description[2];
+
+            if (!dict.ContainsKey(parent))
+            {
+                dict[parent] = new TreeNode(parent);
+            }
+            if (!dict.ContainsKey(child))
+            {
+                dict[child] = new TreeNode(child);
+            }
+
+            if (isleft == 1)
+            {
+                dict[parent].left = dict[child];
+            }
+            else
+            {
+                dict[parent].right = dict[child];
+            }
+        }
+
+        return dict[descriptions.Select(description => description[0]).Except(descriptions.Select(description => description[1])).Single()];
+    }
+}

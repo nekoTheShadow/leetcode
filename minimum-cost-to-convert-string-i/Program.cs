1f8759c64145b416f52e164f0b36ea3f4abae38e
neko_the_shadow <h.nakamura0903@gmail.com>
2024-07-27 23:44:18 +0900

    2976. Minimum Cost to Convert String I

---
+++ minimum-cost-to-convert-string-i/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine(new minimum_cost_to_convert_string_i.Solution().MinimumCost("abcd", "acbe", new char[] { 'a', 'b', 'c', 'c', 'e', 'd' }, new char[] { 'b', 'c', 'b', 'e', 'b', 'e' }, new int[] { 2, 5, 5, 1, 2, 20 }));
+
+Console.WriteLine(new minimum_cost_to_convert_string_i.Solution().MinimumCost("aaaa", "bbbb", new char[] { 'a', 'c' }, new char[] { 'c', 'b' }, new int[] { 1, 2 }));
+
+Console.WriteLine(new minimum_cost_to_convert_string_i.Solution().MinimumCost("abcd", "abce", new char[] { 'a' }, new char[] { 'e' }, new int[] { 10000 }));
---
+++ minimum-cost-to-convert-string-i/Solution.cs
@@ -0,0 +1,52 @@
+﻿namespace minimum_cost_to_convert_string_i;
+
+public class Solution
+{
+    public long MinimumCost(string source, string target, char[] original, char[] changed, int[] cost)
+    {
+        var inf = long.MaxValue / 2 - 1;
+
+        var dist = new long[26, 26];
+        for (var i = 0; i < 26; i++)
+        {
+            for (var j = 0; j < 26; j++)
+            {
+                if (i == j)
+                {
+                    dist[i, j] = 0;
+                }
+                else
+                {
+                    dist[i, j] = inf;
+                }
+
+            }
+        }
+        for (var i = 0; i < original.Length; i++)
+        {
+            dist[original[i] - 'a', changed[i] - 'a'] = Math.Min(dist[original[i] - 'a', changed[i] - 'a'], cost[i]);
+        }
+
+        for (var k = 0; k < 26; k++)
+        {
+            for (var i = 0; i < 26; i++)
+            {
+                for (var j = 0; j < 26; j++)
+                {
+                    dist[i, j] = Math.Min(dist[i, j], dist[i, k] + dist[k, j]);
+                }
+            }
+        }
+
+        long ret = 0;
+        for (var i = 0; i < source.Length; i++)
+        {
+            if (dist[source[i] - 'a', target[i] - 'a'] == inf)
+            {
+                return -1;
+            }
+            ret += dist[source[i] - 'a', target[i] - 'a'];
+        }
+        return ret;
+    }
+}

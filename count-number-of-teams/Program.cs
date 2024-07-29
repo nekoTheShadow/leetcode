092fc599d8c7f56cbad5e160ef0f36a940af8071
neko_the_shadow <h.nakamura0903@gmail.com>
2024-07-29 23:14:32 +0900

    1395. Count Number of Teams

---
+++ count-number-of-teams/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace count_number_of_teams;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var s = new Solution();
+        Console.WriteLine(s.NumTeams(new int[] { 2, 5, 3, 4, 1 }));
+        Console.WriteLine(s.NumTeams(new int[] { 2, 1, 3 }));
+        Console.WriteLine(s.NumTeams(new int[] { 1, 2, 3, 4 }));
+    }
+}
---
+++ count-number-of-teams/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace count_number_of_teams;
+public class Solution
+{
+    public int NumTeams(int[] rating)
+    {
+        int s1 = Solve(rating);
+        Array.Reverse(rating);
+        return s1 + Solve(rating);
+    }
+
+    private int Solve(int[] rating)
+    {
+        var n = rating.Length;
+        var dp = new int[n, 3];
+        for (var i = 0; i < n; i++)
+        {
+            dp[i, 0] = 1;
+        }
+
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = i+1; j < n; j ++)
+            {
+                if (rating[i] < rating[j])
+                {
+                    dp[j, 1] += dp[i, 0];
+                    dp[j, 2] += dp[i, 1];
+                }
+            }
+        }
+        return Enumerable.Range(0, n).Sum(i => dp[i, 2]);
+    } 
+}

5186c8fd9766d71a7e5492ae31810ed27e122a6c
neko_the_shadow <h.nakamura0903@gmail.com>
2024-08-04 12:31:13 +0900

    1508. Range Sum of Sorted Subarray Sums

---
+++ range-sum-of-sorted-subarray-sums/Program.cs
@@ -0,0 +1,4 @@
+﻿using range_sum_of_sorted_subarray_sums;
+Console.WriteLine(new Solution().RangeSum(new int[] { 1, 2, 3, 4 }, 4, 1, 5));
+Console.WriteLine(new Solution().RangeSum(new int[] { 1, 2, 3, 4 }, 4, 3, 4));
+Console.WriteLine(new Solution().RangeSum(new int[] { 1, 2, 3, 4 }, 4, 1, 10));
---
+++ range-sum-of-sorted-subarray-sums/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace range_sum_of_sorted_subarray_sums;
+
+public class Solution
+{
+    public int RangeSum(int[] nums, int n, int left, int right)
+    {
+        var totals = new List<int>();
+        for (int i = 0; i < n; i++)
+        {
+            int total = 0;
+            for (int j = i; j < n; j++)
+            {
+                
+                total += nums[j];
+                totals.Add(total);
+            }
+        }
+
+        totals.Sort();
+
+        int ret = 0;
+        for (int i = left - 1; i < right; i++)
+        {
+            ret += totals[i];
+            ret %= 1000000000 + 7;
+        }
+        return ret;
+    }
+}

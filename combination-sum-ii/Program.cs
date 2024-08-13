936f10277d4312de74cc7f08dde6f0244272dbfb
neko_the_shadow <h.nakamura0903@gmail.com>
2024-08-13 19:34:04 +0900

    40. Combination Sum II

---
+++ combination-sum-ii/Program.cs
@@ -0,0 +1,12 @@
+﻿var s = new combination_sum_ii.Solution();
+
+foreach(var item in s.CombinationSum2(new int[] { 10, 1, 2, 7, 6, 1, 5 }, 8))
+{
+    Console.WriteLine(String.Join(", ", item));
+}
+
+
+foreach (var item in s.CombinationSum2(new int[] { 2, 5, 2, 1, 2 }, 5))
+{
+    Console.WriteLine(String.Join(", ", item));
+}
---
+++ combination-sum-ii/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace combination_sum_ii;
+public class Solution
+{
+    public IList<IList<int>> CombinationSum2(int[] candidates, int target)
+    {
+        var pairs = candidates.GroupBy(x => x).Select(t => (t.Key, t.Count())).ToList();
+        var ret = new List<IList<int>>();
+        dfs(ret, pairs, new Stack<(int, int)>(), target, 0);
+        return ret;
+    }
+
+
+    public void dfs(IList<IList<int>> ret, List<(int, int)> pairs, Stack<(int, int)> stack, int target, int cur)
+    {
+        if (target == 0)
+        {
+            ret.Add(stack.SelectMany(t => Enumerable.Repeat(t.Item1, t.Item2)).ToList());
+            return;
+        }
+
+        if (pairs.Count == cur)
+        {
+            return;
+        }
+
+        var (x, c) = pairs[cur];
+        for (int i = 1; i <= c; i++)
+        {
+            if (target - x * i >= 0)
+            {
+                stack.Push((x, i));
+                dfs(ret, pairs, stack, target - x * i, cur + 1);
+                stack.Pop();
+            }
+        }
+        dfs(ret, pairs, stack, target, cur + 1);
+    }
+
+}

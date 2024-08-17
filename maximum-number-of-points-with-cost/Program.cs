3644292d13e278718b9c7e6f88275fd1c543ab92
neko_the_shadow <h.nakamura0903@gmail.com>
2024-08-18 00:43:36 +0900

    1937. Maximum Number of Points with Cost

---
+++ maximum-number-of-points-with-cost/Program.cs
@@ -0,0 +1,16 @@
+﻿using maximum_number_of_points_with_cost;
+
+void Example01()
+{
+    var points = new int[][] { new int[] { 1, 2, 3 }, new int[] { 1, 5, 1 }, new int[] { 3, 1, 1 } };
+    Console.WriteLine(new Solution().MaxPoints(points));
+}
+
+void Example02()
+{
+    var points = new int[][] { new int[] { 1, 5 }, new int[] { 2, 3 }, new int[] { 4, 2 } };
+    Console.WriteLine(new Solution().MaxPoints(points));
+}
+
+Example01();
+Example02();
---
+++ maximum-number-of-points-with-cost/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace maximum_number_of_points_with_cost;
+public class Solution
+{
+    public long MaxPoints(int[][] points)
+    {
+        var n = points[0].Length;
+        var dp = new long[n];
+
+        foreach (var row in points)
+        {
+            var l = new long[n];
+            var r = new long[n];
+            var lmax = 0L;
+            var rmax = 0L;
+            for (int i = 0; i < n; i++)
+            {
+                lmax = Math.Max(lmax - 1, dp[i]);
+                l[i] = lmax;
+            }
+            for (int i = n - 1; i >= 0; i--)
+            {
+                rmax = Math.Max(rmax - 1, dp[i]);
+                r[i] = rmax;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                dp[i] = Math.Max(l[i], r[i]) + row[i];
+            }
+        }
+
+        return dp.Max();
+    }
+}

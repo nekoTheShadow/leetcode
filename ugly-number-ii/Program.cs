07ec4609dfb4c0c1e9928f41b7b482fac44a155d
neko_the_shadow <h.nakamura0903@gmail.com>
2024-08-18 13:35:45 +0900

    264. Ugly Number II

---
+++ ugly-number-ii/Program.cs
@@ -0,0 +1,6 @@
+﻿using ugly_number_ii;
+
+var s = new Solution();
+Console.WriteLine(s.NthUglyNumber(10));
+Console.WriteLine(s.NthUglyNumber(1));
+Console.WriteLine(s.NthUglyNumber(1690));
---
+++ ugly-number-ii/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace ugly_number_ii;
+
+public class Solution
+{
+    public int NthUglyNumber(int n)
+    {
+        var pq = new PriorityQueue<(long, long), long>();
+        pq.Enqueue((1, 1), 1);
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            var (last, x) = pq.Dequeue();
+            foreach (var y in new int[] { 2, 3, 5 })
+            {
+                if (last <= y)
+                {
+                    pq.Enqueue((y, y * x), y * x);
+                }
+            }
+        }
+
+        return (int)pq.Peek().Item2;
+    }
+}

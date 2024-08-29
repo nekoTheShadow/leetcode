a85583198cb637d353526abea04058ebb25e6563
neko_the_shadow <h.nakamura0903@gmail.com>
2024-08-29 11:30:57 +0900

    947. Most Stones Removed with Same Row or Column

---
+++ most-stones-removed-with-same-row-or-column/Program.cs
@@ -0,0 +1,15 @@
+﻿using most_stones_removed_with_same_row_or_column;
+using System.Text.RegularExpressions;
+
+int[][] ToMatrix(string s)
+{
+    return Regex.Matches(s, @"\[([0-9]*),([0-9]*)\]")
+        .Cast<Match>()
+        .Select(m => new int[] { int.Parse(m.Groups[1].Value), int.Parse(m.Groups[2].Value) })
+        .ToArray();
+}
+
+var s = new Solution();
+Console.WriteLine(s.RemoveStones(ToMatrix("[[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]")));
+Console.WriteLine(s.RemoveStones(ToMatrix("[[0,0],[0,2],[1,1],[2,0],[2,2]]")));
+Console.WriteLine(s.RemoveStones(ToMatrix("[[0,0]]")));
---
+++ most-stones-removed-with-same-row-or-column/Solution.cs
@@ -0,0 +1,51 @@
+﻿namespace most_stones_removed_with_same_row_or_column;
+public class Solution
+{
+    public int RemoveStones(int[][] stones)
+    {
+        int n = stones.Length;
+
+        var uf = new UnionFind(n);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1])
+                {
+                    uf.Union(i, j);
+                }
+            }
+        }
+
+        return n - Enumerable.Range(0, n).Select(x => uf.Find(x)).Distinct().Count();
+    }
+}
+
+public class UnionFind
+{
+    private int[] parents;
+
+    public UnionFind(int n)
+    {
+        this.parents = Enumerable.Range(0, n).ToArray();
+    }
+
+    public int Find(int x)
+    {
+        if (this.parents[x] != x)
+        {
+            this.parents[x] = this.Find(this.parents[x]);
+        }
+        return this.parents[x];
+    }
+
+    public void Union(int x, int y)
+    {
+        x = this.Find(x);
+        y = this.Find(y);
+        if (x != y)
+        {
+            this.parents[x] = y;
+        }
+    }
+}

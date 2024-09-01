f905adcac5f53b24d12834d1cb78e6bddbf0b32e
neko_the_shadow <h.nakamura0903@gmail.com>
2024-09-01 13:23:26 +0900

    2022. Convert 1D Array Into 2D Array

---
+++ convert-1d-array-into-2d-array/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using convert_1d_array_into_2d_array;
+
+void prettyPrint(int[][] a)
+{
+    if (a.Length == 0)
+    {
+        Console.WriteLine("[]");
+    }
+
+    foreach (int[] row in a)
+    {
+        Console.WriteLine("[" + String.Join(", ", row) + "]");
+    }
+}
+
+var s = new Solution();
+prettyPrint(s.Construct2DArray(new int[] { 1, 2, 3, 4 }, 2, 2));
+prettyPrint(s.Construct2DArray(new int[] { 1, 2, 3 }, 1, 3));
+prettyPrint(s.Construct2DArray(new int[] { 1, 2 }, 1, 1));
---
+++ convert-1d-array-into-2d-array/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace convert_1d_array_into_2d_array;
+
+public class Solution
+{
+    public int[][] Construct2DArray(int[] original, int m, int n)
+    {
+        int len = original.Length;
+        if (len != m * n)
+        {
+            return new int[][] { };
+        }
+
+        int[][] a = new int[m][];
+        for (int x = 0; x < m; x++)
+        {
+            a[x] = new int[n];
+        }
+        for (int x = 0; x < len; x++)
+        {
+            a[x / n][x % n] = original[x];
+        }
+        return a;
+    }
+}

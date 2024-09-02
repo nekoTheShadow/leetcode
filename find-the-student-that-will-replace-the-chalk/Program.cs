c399aef58e96dece19ad737ff2328184b8cb3998
neko_the_shadow <h.nakamura0903@gmail.com>
2024-09-02 21:55:42 +0900

    1894. Find the Student that Will Replace the Chalk

---
+++ find-the-student-that-will-replace-the-chalk/Program.cs
@@ -0,0 +1,5 @@
+﻿using find_the_student_that_will_replace_the_chalk;
+
+var s = new Solution();
+Console.WriteLine(s.ChalkReplacer(new int[] { 5, 1, 5 }, 22));
+Console.WriteLine(s.ChalkReplacer(new int[] { 3, 4, 1, 2 }, 25));
---
+++ find-the-student-that-will-replace-the-chalk/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+namespace find_the_student_that_will_replace_the_chalk;
+public class Solution
+{
+    public int ChalkReplacer(int[] chalk, int k)
+    {
+        var vals = chalk.Select(c => new BigInteger(c)).ToList();
+        var x = new BigInteger(k) % vals.Aggregate((acc, val) => acc + val);
+
+        int i = 0;
+        while (vals[i] <= x)
+        {
+            x -= vals[i];
+            i++;
+        }
+        return i;
+    }
+}

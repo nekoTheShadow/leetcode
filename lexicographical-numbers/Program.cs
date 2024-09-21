c5279c997fac8554146d97e68f30df0386845e22
neko_the_shadow <h.nakamura0903@gmail.com>
2024-09-21 21:14:04 +0900

    386. Lexicographical Numbers

---
+++ lexicographical-numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿using lexicographical_numbers;
+
+void Run(int n)
+{
+    var s = new Solution();
+    var numbers = s.LexicalOrder(n);
+    var line = String.Join(",", numbers);
+    Console.WriteLine(line);
+}
+
+
+
+Run(13);
+Run(2);
---
+++ lexicographical-numbers/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace lexicographical_numbers;
+
+public class Solution
+{
+    public IList<int> LexicalOrder(int n)
+    {
+        this.numbers = new List<int>();
+        this.n = n;
+
+        for (int i = 1; i < 10; i++)
+        {
+            this.Dfs(i);
+        }
+
+        return this.numbers;
+    }
+
+    private List<int> numbers;
+    private int n;
+
+    private void Dfs(int x)
+    {
+        if (n < x)
+        {
+            return;
+        }
+
+        this.numbers.Add(x);
+        for (int i = 0; i < 10; i++)
+        {
+            this.Dfs(x * 10 + i);
+        }
+    }
+}

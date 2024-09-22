0cccfb4f7ef798a041987be0145aebafa3fa86a7
neko_the_shadow <h.nakamura0903@gmail.com>
2024-09-22 14:45:19 +0900

    440. K-th Smallest in Lexicographical Order

---
+++ k-th-smallest-in-lexicographical-order/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using k_th_smallest_in_lexicographical_order;
+
+Console.WriteLine(new Solution().FindKthNumber(13, 2));
+Console.WriteLine(new Solution().FindKthNumber(1, 1));
+Console.WriteLine(new Solution().FindKthNumber(4289384, 1922239));
+Console.WriteLine(new Solution().FindKthNumber(804289384, 42641503));
---
+++ k-th-smallest-in-lexicographical-order/Solution.cs
@@ -0,0 +1,42 @@
+﻿namespace k_th_smallest_in_lexicographical_order;
+
+public class Solution
+{
+    public int FindKthNumber(int n, int k)
+    {
+        return (int)Solve(n, k);
+    }
+
+    private long Solve(long n, long k)
+    {
+        long curr = 1;
+        k--;
+        while (k > 0)
+        {
+            long step = CountStep(n, curr, curr + 1);
+            if (step <= k)
+            {
+                curr++;
+                k -= step;
+            }
+            else
+            {
+                curr *= 10;
+                k--;
+            }
+        }
+        return curr;
+    }
+
+    private long CountStep(long n, long p1, long p2)
+    {
+        long step = 0;
+        while (p1 <= n)
+        {
+            step += Math.Min(n + 1, p2) - p1;
+            p1 *= 10;
+            p2 *= 10;
+        }
+        return step;
+    }
+}

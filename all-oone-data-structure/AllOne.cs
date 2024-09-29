6a12af912c3aa75f144832ec05bc52187d6f30dc
neko_the_shadow <h.nakamura0903@gmail.com>
2024-09-29 21:09:37 +0900

    432. All O`one Data Structure

---
+++ all-oone-data-structure/AllOne.cs
@@ -0,0 +1,80 @@
+﻿namespace all_oone_data_structure;
+
+public class AllOne
+{
+    private Dictionary<string, int> counts;
+    private SortedDictionary<int, HashSet<string>> words;
+    public AllOne()
+    {
+        this.counts = new Dictionary<string, int>();
+        this.words = new SortedDictionary<int, HashSet<string>>();
+    }
+
+    public void Inc(string key)
+    {
+        int count = this.counts.GetValueOrDefault(key, 0);
+        this.counts[key] = count + 1;
+        this.Move(count, count + 1, key);
+    }
+
+    public void Dec(string key)
+    {
+        int count = this.counts[key];
+        if (count == 1)
+        {
+            this.counts.Remove(key);
+        }
+        else
+        {
+            this.counts[key] = count - 1;
+        }
+        this.Move(count, count - 1, key);
+    }
+
+    public string GetMaxKey()
+    {
+        if (this.IsEmpty())
+        {
+            return "";
+        }
+        return this.words.Last().Value.First();
+    }
+
+    public string GetMinKey()
+    {
+        if (this.IsEmpty())
+        {
+            return "";
+        }
+
+        return this.words.First().Value.First();
+    }
+
+    private bool IsEmpty()
+    {
+        return this.counts.Count == 0;
+    }
+
+    private void Move(int count1, int count2, string word)
+    {
+        if (this.words.ContainsKey(count1))
+        {
+            this.words[count1].Remove(word);
+            if (this.words[count1].Count == 0)
+            {
+                this.words.Remove(count1);
+            }
+        }
+
+        if (!this.words.ContainsKey(count2))
+        {
+            this.words[count2] = new HashSet<string>();
+        }
+        this.words[count2].Add(word);
+
+        if (this.words.ContainsKey(0))
+        {
+            this.words.Remove(0);
+        }
+    }
+}
---
+++ all-oone-data-structure/Program.cs
@@ -0,0 +1,10 @@
+﻿using all_oone_data_structure;
+
+var allOne = new AllOne();
+allOne.Inc("hello");
+allOne.Inc("hello");
+Console.WriteLine(allOne.GetMaxKey());
+Console.WriteLine(allOne.GetMinKey());
+allOne.Inc("leet");
+Console.WriteLine(allOne.GetMaxKey());
+Console.WriteLine(allOne.GetMinKey());
